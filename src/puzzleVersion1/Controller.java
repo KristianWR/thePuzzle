d7bebe3d686aa2ec09d3aecd4591670db2b0a952
Pelle <pelle.galloe@gmail.com>
2016-01-05 18:19:15 +0100

    update

--- src/puzzleVersion1/Controller.java
+++ src/puzzleVersion1/Controller.java
@@ -1,5 +1,29 @@
 package puzzleVersion1;
 
+import javafx.scene.control.Button;
+
 public class Controller {
+	public Puzzle currentPuzzle;
 	
+	public Controller(int size){
+		currentPuzzle = new Puzzle(size);
+	}
+	
+	public String validMove(String btnID){
+		return "hej";
+	}
+	
+	public void checkMove(int hej){
+		Button[][] movable = currentPuzzle.getButtons();
+		
+		String valid = validMove("hej");
+		if (String == "left"){		
+			
+		}//else if ()
+		
+		//change [i][j] with [j][i]
+		
+		currentPuzzle.changeBtns(movable);
+	}		
+		
 }
--- src/puzzleVersion1/Puzzle.java
+++ src/puzzleVersion1/Puzzle.java
@@ -1,10 +1,35 @@
 package puzzleVersion1;
-	
+
+import javafx.scene.control.Button;
+
 public class Puzzle {
+	
+	Button[][] btns;
+	int puzzleSize;
+	
+	//constructs a btn 2 dimensional array with the int size
 	public Puzzle (int size){
-		int[] numbers = new int[size];
+		btns = new Button[size][size];
+		puzzleSize = size;
+		int count = 0;
 		for (int i = 0; i<size; i++){
-			numbers[i] = i;
+			for (int j = 0; j<size; j++){
+				btns[i][j] = new Button(Integer.toString(count));
+				count++;
+			}
 		}
 	}
+	
+	public Button[][] getButtons(){
+		return btns;
+	}
+	public void changeBtns(Button[][] changed){
+		btns = changed;
+	}
+	
+	public int getSize(){
+		return puzzleSize;
+	}
+	
+	
 }
