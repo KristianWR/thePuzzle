8e7d4bcf3f4bfa27e4bb0139e194caf271f54e92
kristianrasmussen1 <kr94@live.dk>
2016-01-15 20:18:52 +0100

    Kommenteret about

--- src/puzzleV3/AboutScene.java
+++ src/puzzleV3/AboutScene.java
@@ -1,5 +1,6 @@
 package puzzleV3;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -24,29 +25,35 @@ public class AboutScene {
 	public void aboutSceneM(){
 		BorderPane border = new BorderPane();
 		GridPane grid = new GridPane();
+		Label dev = new Label("The game developers:");
+		Label tekst = new Label("Kristian Wolthers Rasmussen (s154121) \nPelle Rubin Galloee (s153742) \nRasmus Liebst (s144483) "
+	    		+ "\nJia Johnny Ye (s154074)");
+		
+		//Sets the horizontal and vertical gap between grids and sets the position of the grid
 	    grid.setHgap(10);
 	    grid.setVgap(20);
 	    grid.setAlignment(Pos.TOP_CENTER);
 	    
+	    // Adds the id references to the labels. This id is called in the css file (screen2.css)
+	    dev.getStyleClass().add("label-fed");
+	    tekst.getStyleClass().add("label-about");
+	    
+	    //Adds elements to grid with the given location
+	    grid.add(dev, 0, 0);
+	    grid.add(tekst, 0, 1);
+	    
+	    // Sets the style, max size and position of the back button
 		back.setMaxWidth(40);
 		back.getStyleClass().add("button-back");
 		back.setAlignment(Pos.TOP_LEFT);
 	    
-		//Tekst on about menu
-	    Label dev = new Label("The game developers:");
-	    dev.getStyleClass().add("label-fed");
-	    Label tekst = new Label("Kristian Wolthers Rasmussen (s154121) \nPelle Rubin Galloee (s153742) \nRasmus Liebst (s144483) "
-	    		+ "\nJia Johnny Ye (s154074)");
-	    tekst.getStyleClass().add("label-about");
-	    
-	    grid.add(dev, 0, 0);
-	    grid.add(tekst, 0, 1);
-	    
 	    border.setTop(back);
 	    border.setCenter(grid);
+	    border.setPadding(new Insets(15,15,15,15));
 		
 	    aboutScene = new Scene(border, 750, 750);
 		aboutStage.setScene(aboutScene);
+		
 		//Fetches the .css-file that styles the GUI elements from the same place where View class is
 		aboutScene.getStylesheets().add(View.class.getResource("screen2.css").toExternalForm());
 	}
