330c04655a911d01d85561d8e9f89b67873ec840
kristianrasmussen1 <kr94@live.dk>
2016-01-15 21:34:38 +0100

    AlertBox struktureret samt kommenteret

--- src/puzzleV3/AlertBox.java
+++ src/puzzleV3/AlertBox.java
@@ -12,58 +12,54 @@ public class AlertBox {
 	Button mainMenuButton;
 	Button restartButton;
 	Stage window;
-	
-	public AlertBox(){
-		//Creates two new buttons
+
+	public AlertBox() {
 		mainMenuButton = new Button("Main Menu");
 		restartButton = new Button("Try again");
 		window = new Stage();
-		
-		//Forces you to handle the pop-up window
+
+		// Forces you to handle the pop-up window
 		window.initModality(Modality.APPLICATION_MODAL);
 
 	}
-	
-    public void display(String title, String message) {
-       // window = new Stage();
 
-        //Block events to other windows
-        window.setTitle(title);
-        window.setMinWidth(250);
-        window.setMinHeight(250);
+	public void display(String title, String message) {
+		Label label = new Label();
+		VBox layout = new VBox(10);
+		Scene scene = new Scene(layout, 250, 250);
 
-        //Creates a new label
-        Label label = new Label();
-        label.setText(message);
-        
-        VBox layout = new VBox(10);
-        //Adds elements to layout
-        layout.getChildren().addAll(label,restartButton, mainMenuButton);
-        layout.setAlignment(Pos.CENTER);
+		label.setText(message);
 
-        //Display window and wait for it to be closed before returning
-        Scene scene = new Scene(layout, 250, 250);
-        
-		//Tells eclipse to look in the same package as view and use the Alert file
+		// Adds elements to the VBox layout
+		layout.getChildren().addAll(label, restartButton, mainMenuButton);
+		layout.setAlignment(Pos.CENTER);
+
+		window.setTitle(title);
+		window.setMinWidth(250);
+		window.setMinHeight(250);
+		window.setScene(scene);
+		window.show();
+
+		// Tells the window to exit program when x is pressed
+		window.setOnCloseRequest(e -> Platform.exit());
+
+		// Tells eclipse to look in the same package as view and use the Alert file
 		scene.getStylesheets().add(View.class.getResource("Alert.css").toExternalForm());
-        window.setScene(scene);
-        window.show();
-        
-        //Tells the window to exit program when x is pressed
-        window.setOnCloseRequest(e -> Platform.exit());
-    }
-    //This is a get-method which returns the mainMenuButton
-    public Button getMainMenuButton() {
-    	return mainMenuButton;
-    }
-    //This is a get-method which returns the restartButton
-    public Button getRestartButton(){
-    	return restartButton;
-    }
-    //This is a get-method which returns the window
-    public Stage getAlertBoxWindow(){
-    	return window;
-    }
+	}
 
+	// This is a get-method which returns the button mainMenuButton
+	public Button getMainMenuButton() {
+		return mainMenuButton;
+	}
+
+	// This is a get-method which returns the button restartButton
+	public Button getRestartButton() {
+		return restartButton;
+	}
+
+	// This is a get-method which returns the Stage window
+	public Stage getAlertBoxWindow() {
+		return window;
+	}
 
 }
