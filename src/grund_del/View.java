99601fab67f4f8c53e9a83f9e81bc6aa24ac9691
Johnny <Johnny@10.128.65.46>
2016-01-15 18:43:07 +0100

    Grunddel færdig

---
+++ src/grund_del/View.java
@@ -0,0 +1,125 @@
+package grund_del;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+public class View extends Application{
+	Scene game;
+	model m;
+	Label[][] labels;
+	int initSize;
+	TextField sizePrompt;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		
+		//Creates gridpane
+				GridPane grid2 = new GridPane();
+				grid2.setAlignment(Pos.TOP_CENTER);
+				
+				Scene sizePicker= new Scene(grid2, 700, 700);
+
+				Label chooseSize = new Label("Choose the size of the game");
+				chooseSize.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
+				
+				//Adds choosesize with location on grid
+				GridPane.setConstraints(chooseSize, 1, 2);
+				
+				sizePrompt = new TextField();
+				
+				//Adds sizeprompt with location on grid
+				GridPane.setConstraints(sizePrompt, 1, 3);
+				
+				Label sizeInfo = new Label("(N.B. write a number between 3 and 100)");
+				
+				//Adds sizeInfo with location on grid
+				GridPane.setConstraints(sizeInfo, 1, 4);
+				
+				//Creates buttons with location on grid
+				Button go=new Button("start");
+				GridPane.setConstraints(go, 2, 6);
+				
+				go.setOnMouseClicked(e->{
+					
+					//initSize = Integer.parseInt(sizePrompt.getText());
+					gameSceneM();					
+
+					
+				});
+
+				//Adds elements to grid
+				grid2.getChildren().addAll(chooseSize, sizePrompt, sizeInfo, go);
+				primaryStage.setScene(sizePicker);
+				primaryStage.show();
+}
+	
+	//Creates all the labels on gridpane
+	public GridPane addLabels(GridPane gridPane, Label[][] tempLabels){
+		for(int i = 0; i < tempLabels.length; i++){
+			for(int j = 0; j < tempLabels.length; j++){
+					GridPane.setConstraints(tempLabels[i][j], j, i);
+					gridPane.getChildren().add(tempLabels[i][j]);
+					}
+			}
+		return gridPane;
+	}
+		
+	public void gameSceneM(){
+		
+		//Makes an instance of model
+		Stage window = new Stage();
+		initSize = Integer.parseInt(sizePrompt.getText());
+		m= new model();
+		m.createLabels(initSize);
+		labels= model.getLabels();
+		
+		//Adds the generated labels to gridpane
+		GridPane gridPane = new GridPane();
+		gridPane =addLabels(gridPane, labels);
+		gridPane.setMaxSize(600, 600);
+		gridPane.setAlignment(Pos.CENTER);
+		
+		//// SCROLLPANE --- The layout that has the scroll feature for the game layout ///
+		
+		ScrollPane scroll1 = new ScrollPane(gridPane);
+		scroll1.setFitToWidth(true);
+	
+		//Adds game and scrollpane to new gridpane
+		GridPane mainGrid= new GridPane();
+		mainGrid.setAlignment(Pos.CENTER);
+		mainGrid.add(gridPane, 1, 1);
+		mainGrid.add(scroll1, 0, 1);
+		
+		game=new Scene(mainGrid, 1000, 1000);
+		addArrowKeyListener();
+		window.setScene(game);
+		window.show();
+		
+	}
+	
+	public void addArrowKeyListener(){
+		game.setOnKeyPressed(e ->{
+			if		(e.getCode() == KeyCode.UP){m.keyMove("down");}
+			else if (e.getCode() == KeyCode.DOWN){m.keyMove("up");}
+			else if (e.getCode() == KeyCode.LEFT){m.keyMove("right");}
+			else if (e.getCode() == KeyCode.RIGHT){m.keyMove("left");}
+		}); 
+	}
+	
+	
+}
---
+++ src/grund_del/model.java
@@ -0,0 +1,163 @@
+package grund_del;
+
+import java.awt.Point;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+
+public class model {
+	
+    static Label[][] labels;
+	int puzzleSize;
+	Label moveCount = new Label("0");
+	
+	//Only used for communicating to the view that a special change occurs.
+	Label isPlaying = new Label("yes"); 
+	
+	//A point that keeps track of the label with the text "0". used in our randomMove().
+	Point zeroPos = new Point(0,0);
+	
+	public static  Label[][] getLabels(){
+		return labels;
+	}
+	
+	/*
+	 *Creates the 2 dimensional labelArray that represents the game board.
+	 *It scales the label size to be optimal with our scene. It changes the 
+	 *first 3 labels to be in order 2, 3, 1 and 0 to be the in the bottom
+	 *right corner. and "hides" the label with text "0".  
+	 */
+	public void createLabels(int size){
+		//Initializes the label array with the correct dimensions.
+		isPlaying.setText("yes");
+		labels = new Label[size][size];
+		puzzleSize = size;
+	
+		//Used to define the size of the labels. 
+				String amountOfDigits = Integer.toString(size*size);
+				double dSize = (amountOfDigits.length()*5)+20;
+		
+		//Sets properties for each label in labels[][] with the text of the
+		//Current count, starting with 1.
+		int count = 1;
+		for (int i = 0; i<size; i++){
+			for (int j = 0; j<size; j++){
+				labels[i][j] = new Label(Integer.toString(count));
+				labels[i][j].setMinSize(dSize, dSize);
+				labels[i][j].setAlignment(Pos.CENTER);
+				count++;
+			}
+		}
+		
+		//Sets the bottom right labels' text to "0" and adds the css style
+		labels[size-1][size-1].setText("0");
+		labels[size-1][size-1].setVisible(false);
+
+		
+		//Sets the first 3 labels in sequence 2, 3, 1
+		Label holder1 = labels[0][0];
+		Label holder2 = labels[0][1];
+		labels[0][0] = labels[0][2];	labels[0][2] = holder1;
+		labels[0][1] = labels[0][0];	labels[0][0] = holder2;
+		
+		//Sets the mouseClickAction for every label in labels
+		for (int i = 0; i<labels.length; i++){
+			for (int j = 0; j<labels.length; j++){
+				setMouseClickAction(i, j);
+			}
+		}
+		//Important variable for the randomMove() method.
+		zeroPos.setLocation(size-1, size-1);
+		moveCount.setText("0");
+	}
+	
+	/*
+	 * Checks if the label at the given x,y in labels is next to zero.
+	 * It returns where the zero is relative to the given values.
+	 */
+	public void checkMove(int x, int y){
+		String var = nextToZero(x, y);
+
+		//Runs through every possible outcome of nextToZero()
+		// and switch labels accordingly.
+		if (var == "up"){
+			switchLabels(y-1, x, y, x);
+		}else if (var == "down"){
+			switchLabels( y+1, x, y, x);
+		}else if (var == "left"){
+			switchLabels( y, x-1, y, x);
+		}else if (var == "right"){
+			switchLabels( y, x+1, y, x);
+		}else if (var == "not"){
+			System.out.println("not a valid move");
+		}
+	}
+	/*
+	 * This method is called when a key is pressed. opposite key. e.g up becomes down.
+	 * it get's the x, y of the label with text "0" and moves the label in the direction.
+	 */
+	public void keyMove(String command){
+		int x = zeroPos.x;
+		int y = zeroPos.y;
+		if 		(command.equals("up")    && y>0){checkMove(x, y-1);}
+		else if (command.equals("down")  && y<puzzleSize-1){checkMove(x, y+1);}
+		else if (command.equals("left")  && x>0){checkMove(x-1, y);}
+		else if (command.equals("right") && x<puzzleSize-1){checkMove(x+1,y);}
+	}
+	/*
+	 * Checks the labels in each of the 4 directions from the labels' 
+	 * x,y coordinates sent along. It only checks inside our labels[][].
+	 * It returns a string with the direction of which the label with "0" is.
+	 */
+	public String nextToZero(int x, int y){
+		//Check up if there is a label "above" the current label in our labels[][]
+		if (y > 0) {
+			if (labels[y - 1][x].getText().equals("0")) {return "up";}}
+		// Check down if there is a label "below" the current label in our labels[][]
+		if (y < (puzzleSize - 1)) {
+			if (labels[y + 1][x].getText().equals("0")) {return "down";}}
+		// Check left if there is a label to the "left of" the current label in our labels[][]
+		if (x > 0) {
+			if (labels[y][x - 1].getText().equals("0")) {return "left";}}
+		// Check right if there is a label to the "right of" the current label in our labels[][]
+		if (x < (puzzleSize - 1)) {
+			if (labels[y][x + 1].getText().equals("0")) {return "right";}}
+		//If not next to zero 
+		return "not";
+	}
+	
+	/*
+	 * Switches the text of 2 labels. updates the position of the "0". 
+	 * It switches the cssStyle that "hides" the label with the text "0".
+	 * Lastly it updates both the moves integer and movesCount label.
+	 */
+	public void switchLabels(int i, int j, int h, int v){
+		//Switches the text on the two labels 
+		String temp = labels[i][j].getText();
+		labels[i][j].setText(labels[h][v].getText());
+		labels[h][v].setText(temp);
+		//Updates the zerPos point
+		zeroPos.setLocation(v, h);
+		
+		/*
+		 * Functionally it makes the label with the text "0" become invisible and updates visibility.
+		 */
+		labels[h][v].setVisible(false);
+	    labels[i][j].setVisible(true);
+		
+	}
+
+
+	/*
+	 * Sets the action to perform when a label is clicked to
+	 * check if it can be moved. If so it moves it, and after every move.
+	 */
+	public void setMouseClickAction(int y, int x){
+		labels[y][x].setOnMouseClicked(e -> {
+			checkMove(x, y);
+		});
+	}
+	
+}
+	
+
